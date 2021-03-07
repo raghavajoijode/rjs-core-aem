1df99b4e7fa1a1032f957aaf70af05281abb0a3e
RJ <raghava.joijode@gmail.com>
2021-03-07 13:09:43 +0530

    Changes

--- ui/apps/src/main/content/jcr_root/apps/rjs-core/clientlibs/clientlib-react/resources/precache-manifest.fa0f205ed72c01929ee9925afb975f72.js
+++ ui/apps/src/main/content/jcr_root/apps/rjs-core/clientlibs/clientlib-react/resources/precache-manifest.fa0f205ed72c01929ee9925afb975f72.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "59bb6d75c73cafb859daa9338fa9e45f",
+    "revision": "42ce06a1c345cc697b3ebea7eaef6efd",
     "url": "/etc.clientlibs/rjs-core/clientlibs/clientlib-react/resources/index.html"
   },
   {
-    "revision": "3b71b4059d7a98438730",
+    "revision": "17b13e50b406a15190c7",
     "url": "/etc.clientlibs/rjs-core/clientlibs/clientlib-react/resources/static/css/main.2a17b812.chunk.css"
   },
   {
-    "revision": "c6c5faeb5215857b50a1",
-    "url": "/etc.clientlibs/rjs-core/clientlibs/clientlib-react/resources/static/js/2.2060571e.chunk.js"
+    "revision": "e2a4d95fad1be766578d",
+    "url": "/etc.clientlibs/rjs-core/clientlibs/clientlib-react/resources/static/js/2.c67cb45c.chunk.js"
   },
   {
     "revision": "a997066f5a8da487bf2934a8b8fd42fb",
-    "url": "/etc.clientlibs/rjs-core/clientlibs/clientlib-react/resources/static/js/2.2060571e.chunk.js.LICENSE.txt"
+    "url": "/etc.clientlibs/rjs-core/clientlibs/clientlib-react/resources/static/js/2.c67cb45c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3b71b4059d7a98438730",
-    "url": "/etc.clientlibs/rjs-core/clientlibs/clientlib-react/resources/static/js/main.ceb8e3ef.chunk.js"
+    "revision": "17b13e50b406a15190c7",
+    "url": "/etc.clientlibs/rjs-core/clientlibs/clientlib-react/resources/static/js/main.9506b2cc.chunk.js"
   },
   {
     "revision": "2fa14443726acf47610f",
